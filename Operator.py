0b95490b4a48823e864303b0a7e0ecfade15b60f
Forkan Amin <226377831+forkanaminshaon@users.noreply.github.com>
2025-09-29 23:55:33 +0600

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,29 @@
+# Addition
+
+a = 10
+b = 20
+print(a + b)
+
+# Subtraction
+print(a - b)
+
+#Multiplicaton
+print(a * b)
+
+#Division
+print(a / b)
+
+#Modulus
+print(a % b)
+
+#Exponential
+
+x = 5
+y = 2
+print(x ** y)
+
+#Floor Division
+sum1 = 15
+sum2 = 2
+print(sum1 // sum2)
+
